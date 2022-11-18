6c118227376ec527adb6965680d39b4a8d5890c6
Claire <claire.kurth@orange.fr>
2022-11-18 13:00:06 +0100

    Affichage sous catégories dans le filtre

--- filtre.php
+++ filtre.php
@@ -1,5 +1,4 @@
-<nav class="nav-filtre">
-    <?php
+<?php
     ini_set('display_errors', TRUE);
     error_reporting(E_ALL);
     //Connexion bdd
@@ -14,17 +13,49 @@
     }
 
     // Renvoie le nom des catégories qui n'ont pas de super-categories --> Aliment
-    $sqlQuery = "SELECT nom FROM categorie WHERE id_categorie NOT IN (SELECT id_categorie FROM possede_spc);";
+    $sqlQuery = "SELECT * FROM categorie WHERE id_categorie NOT IN (SELECT id_categorie FROM possede_spc);";
     $statement = $pdo->prepare($sqlQuery);
     $statement->setFetchMode(PDO::FETCH_ASSOC);
     $statement->execute();
-
     $haut_hierarchie = $statement->fetchAll();
+
     foreach($haut_hierarchie as $row) {
-        //var_dump($row);
-        echo "<span>\n";
+        //print($row['id_categorie']);
+        echo '<a href="index.php">' . ($row['nom']) . '</a>';
+        echo ' > ';
+    }
+   echo '</br></br>';
+    
+?>
+
+
+
+<nav class="nav-filtre">
+    <?php
+    
+    foreach($haut_hierarchie as $row) {
+        //print($row['id_categorie']);
+        echo "<span style=\"color:red\">\n";
         echo ($row['nom']);
         echo "</span>\n";
+        echo "<ul>";
+
+        //Récupère les sous catégories
+        $id = $row['id_categorie'];
+        $sqlQuery = "SELECT * FROM sous_categorie JOIN possede_ssc USING (id_sous_categorie,id_sous_categorie) WHERE id_categorie=$id";
+        $statement = $pdo->prepare($sqlQuery);
+        $statement->setFetchMode(PDO::FETCH_ASSOC);
+        $statement->execute();
+        $sous_cat = $statement->fetchAll();
+        //var_dump($sous_cat);
+
+        foreach($sous_cat as $cat){
+            echo "<li>";
+            echo ($cat['nom']);
+            echo '</li>';
+        }
+        echo "</ul>";
+
     }
     ?>
 </nav>
