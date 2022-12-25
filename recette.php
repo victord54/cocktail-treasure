24a164e936af361aff0362eabbeac057af87b2ec
Claire <claire.kurth@orange.fr>
2022-12-25 15:46:34 +0100

    recette style

--- recette.php
+++ recette.php
@@ -7,7 +7,11 @@
     } else {
         header("Location: index.php");
     }
-
+    //Tableau des caractères à remplacer pour les images
+    $search  = array('À', 'Á', 'Â', 'Ã', 'Ä', 'Å', 'Ç', 'È', 'É', 'Ê', 'Ë', 'Ì', 'Í', 'Î', 'Ï', 'Ò', 'Ó', 'Ô', 'Õ', 'Ö', 'Ù', 'Ú', 'Û', 'Ü', 'Ý', 'à', 'á', 'â', 'ã', 'ä', 'å', 'ç', 'è', 'é', 'ê', 'ë', 'ì', 'í', 'î', 'ï', 'ð', 'ò', 'ó', 'ô', 'õ', 'ö', 'ù', 'ú', 'û', 'ü', 'ý', 'ÿ', 'ñ', ' ', '\'');
+    //Tableau avec les caractères de remplacement pour les images
+    $replace = array('A', 'A', 'A', 'A', 'A', 'A', 'C', 'E', 'E', 'E', 'E', 'I', 'I', 'I', 'I', 'O', 'O', 'O', 'O', 'O', 'U', 'U', 'U', 'U', 'Y', 'a', 'a', 'a', 'a', 'a', 'a', 'c', 'e', 'e', 'e', 'e', 'i', 'i', 'i', 'i', 'o', 'o', 'o', 'o', 'o', 'o', 'u', 'u', 'u', 'u', 'y', 'y', 'n', '_','');
+    
     $login = file_get_contents("data/login");
     $password = file_get_contents("data/password");
     $dbname = 'cocktail_treasure';
@@ -85,23 +89,35 @@ jQuery(document).ready(function($){
 </script>
 
     <?php echo "\n"; include_once("header.php");?>
-    <div>
-    <h1><?php echo $recette['titre'];?></h1>
-    <p>Pour cette recette, vous aurez besoin de :
-        <ul id="liste_ingredients"><?php 
-        foreach ($liste_ingredients as $ingredient){
-            echo '<li> ♣ ' . $ingredient . '</li>';
-        }
-        ?></ul>
-    </p>
-    <p><?php echo $recette['preparation'];?></p>
-    
-    <div>
-        <button id="addFav" <?php if ($fav) { ?> style="display:none" <?php } ?>>Ajouter aux favoris <span id="coeur_vide"> ♡ </span></button>
-        <button id="removeFav" <?php if (!$fav) { ?> style="display:none" <?php } ?>>Enlever des favoris <span id="coeur_plein"> ❤ </span></button>
+    <div id="recette">
+        <h1><?php echo $recette['titre'];?></h1>
+        <?php 
+        $img_titre = str_replace($search, $replace, $recette['titre']);
+        $path = 'resources/photos/' . $img_titre . '.jpg';
+        if (file_exists($path)){
+            echo '<img class="img_recette" src="resources/photos/' . $img_titre .'.jpg" alt="Sample">';
+        } else {
+            echo '<img class="img_recette" src="resources/photos/sample.png" alt="Sample">';
+        }?>
+        <fieldset>
+            <legend>Ingrédients</legend>
+            <p>Pour cette recette, vous aurez besoin de :
+                <ul id="liste_ingredients"><?php 
+                foreach ($liste_ingredients as $ingredient){
+                    echo '<li> ♣ ' . $ingredient . '</li>';
+                }
+                ?></ul>
+            </p>
+        </fieldset>
+        <fieldset>
+            <legend>Etapes</legend>
+            <p><?php echo $recette['preparation'];?></p>
+        </fieldset>
+        <div>
+            <button id="addFav" <?php if ($fav) { ?> style="display:none" <?php } ?>>Ajouter aux favoris <span id="coeur_vide"> ♡ </span></button>
+            <button id="removeFav" <?php if (!$fav) { ?> style="display:none" <?php } ?>>Enlever des favoris <span id="coeur_plein"> ❤ </span></button>
+        </div>
     </div>
-
-</div>
     <?php echo "\n"; include_once("footer.php"); echo "\n";?>
 </body>
 </html>
